18689e8103dcd62e7dbe85b0301976e9032d4ea7
Bünyamin Dagdelen <153297272+dagdelenbunyamin@users.noreply.github.com>
2026-02-24 16:31:22 +0100

    Update metadata title and description in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Dagdelen",
+  description: "Portfolio",
 };
 
 export default function RootLayout({
